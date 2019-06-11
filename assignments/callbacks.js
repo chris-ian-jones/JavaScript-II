f37e8a757153bc5a600a1c98833ac94cdb039be5
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-11 22:12:07 +0100

    Completed: stretch for callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -98,4 +98,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+
+  //the Set object lets you store unique values of any type
+  return cb(new Set(array));
 }
+
+removeDuplicates(items, function(unqiueItems) {
+  console.log(unqiueItems);
+});
